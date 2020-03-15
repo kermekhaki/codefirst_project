f25d87c4e93d798f7c6cf95bb5169bb00bc58d7d
kermekhaki <amona09@gmail.com>
2020-03-15 16:33:15 +0000

    Added task 5, improved task 1

--- sales.py
+++ sales.py
@@ -1,15 +1,22 @@
 import csv
 
-#Task 1 & Task 2
+#Task 1
+# When we use WITH a file is closed as soon as you finished using with.
+# This means that we can't continue using the file as it is closed.
+# We have to read the file and save our data so we can continue using it by creating a new list with our data from the csv file
+my_data = []
 with open('Sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
 
-    sales = []
-
     for row in spreadsheet:
-        #print(row.keys()) gives names of columns
-        Sales_sum = int(row['sales'])
-        sales.append(Sales_sum)
+        my_data.append(row)
+
+
+#Task 2
+sales = []
+for row in my_data:
+    Sales_sum = int(row['sales'])
+    sales.append(Sales_sum)
 
 #Task 3
 print('The total sum of the sales for 2018 is {}.'.format(sum(sales)))
@@ -18,7 +25,13 @@ print('The total sum of the sales for 2018 is {}.'.format(sum(sales)))
 print('The average sale per month for 2018 is {}.'.format(round(sum(sales) / len(sales), 2)))
 
 #Task 5
-print()
+My_minimum = min(sales)
+
+for row in my_data:
+    Sales_minimum = int(row['sales'])
+    if My_minimum == Sales_minimum:
+        print('The minimum sales during 2018 is for the month of {} and is £{}.'.format(row['month'], My_minimum))
+
 
 
 
