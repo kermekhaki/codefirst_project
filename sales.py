0e50144d854791a82c69c97e5854b896dcc672d3
kermekhaki <amona09@gmail.com>
2020-03-15 15:19:33 +0000

    Added Task 3 code

--- sales.py
+++ sales.py
@@ -1,11 +1,16 @@
 import csv
 
+#Task 1 & Task 2
 with open('Sales.csv', 'r') as csv_file:
     spreadsheet = csv.DictReader(csv_file)
 
     sales = []
 
     for row in spreadsheet:
-       # print(row.keys()) gives names of columns
-        Sales_sum = row['sales']
+        #print(row.keys()) gives names of columns
+        Sales_sum = int(row['sales'])
         sales.append(Sales_sum)
+
+#Task 3
+print(str(sum(sales)))
+
